bb21fa31aa2ea99b5db8ba919e3dd5713799b64e
Dhruvil Shah <48760936+jackfrost1411@users.noreply.github.com>
2020-06-13 23:40:40 +0530

    Delete db.js

--- test/db.js
+++
@@ -1,8 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-let dbo;
-let db = MongoClient.connect("mongodb+srv://dhruvil:dhruvil123@cluster0-8obxb.mongodb.net/test?retryWrites=true", {useNewUrlParser:true})
-// function(err, db) {
-// 	dbo = db
-// });
-
-module.export = db;
